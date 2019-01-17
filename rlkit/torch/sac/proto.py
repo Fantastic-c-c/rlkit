fix kl bug

rlkit/torch/sac/proto.py
@@ -12,7 +12,7 @@ def _product_of_gaussians(mus, sigmas_squared):
     compute mu, sigma of product of gaussians
     '''
     # clamp here
-    # sigmas_squared = torch.clamp(sigmas_squared, min=0.0001)
+    sigmas_squared = torch.clamp(sigmas_squared, min=1e-8)
     sigma_squared = 1. / torch.sum(torch.reciprocal(sigmas_squared), dim=0)
     mu = sigma_squared * torch.sum(mus / sigmas_squared, dim=0)
     return mu, torch.sqrt(sigma_squared)