a0093af4b02e769b0111c452f8d17dac7246d9cf
Kate Rakelly <krakelly@berkeley.edu>
2019-04-02 21:38:51 -0700

    point robot shaped reward experiment settings

--- examples/sac_point_robot.py
+++ examples/sac_point_robot.py
@@ -15,21 +15,21 @@ def main(gpu, docker, debug):
     variant = make_variant(max_path_length)
 
     variant['env_name'] = 'point-robot'
-    variant['n_train_tasks'] = 5
-    variant['n_eval_tasks'] = 5
+    variant['n_train_tasks'] = 80
+    variant['n_eval_tasks'] = 20
 
     env_params = variant['env_params']
-    env_params['n_tasks'] = 10
+    env_params['n_tasks'] = 100
 
     algo_params = variant['algo_params']
+    algo_params['num_steps_per_eval'] = 10 * max_path_length
     algo_params['reward_scale'] = 100.
     algo_params['kl_lambda'] = .1
-    algo_params['train_embedding_source'] ='online_exploration_trajectories'
+    algo_params['train_embedding_source'] ='online_on_policy_trajectories'
 
     util_params = variant['util_params']
     util_params['gpu_id'] = gpu
 
-    print(variant)
     experiment(variant)
 
 if __name__ == "__main__":
