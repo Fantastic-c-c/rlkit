104bd46eb3cfa7c1c0185e8bc06694c15b4cede5
Kate Rakelly <krakelly@berkeley.edu>
2019-08-06 10:28:10 -0700

    switch to constant frequency velocity control
    optionally include velocity in state

--- rlkit/envs/sawyer_peg_real.py
+++ rlkit/envs/sawyer_peg_real.py
@@ -5,6 +5,7 @@ from rlkit.core.serializable import Serializable
 import numpy as np
 from gym.spaces import Box
 import logging
+import time
 
 from . import register_env
 
@@ -20,25 +21,29 @@ class MultitaskSawyerPegEnv(Serializable, SawyerReachXYZEnv):
         SawyerReachXYZEnv.__init__(self, config_name='laudri_peg_config', **kwargs)
 
         self.goal_thresh = goal_thresh
-        self.pos_control_reset_position = np.array([0.732, .0195, .267])
+        #self.pos_control_reset_position = np.array([0.732, .0195, .267])
+        # TODO make it easier by starting right above the hole
+        self.pos_control_reset_position = np.array([.745, .045, .295])
         # first axis is rotation about z
         # for sawyer, orientation around y, x, z
         self.reset_orientation = euler_2_quat(0, 0, np.pi)
-        self.reset_pose = np.concatenate((self.pos_control_reset_position, self.reset_orientation))
+        #self.reset_pose = np.concatenate((self.pos_control_reset_position, self.reset_orientation))
+        # TODO remove this for rotation control
+        self.reset_pose = np.concatenate((self.pos_control_reset_position, np.array([.373, .928, -.006, .005])))
 
         # set ee position and angle safety box
         self.ee_angle_lows = np.array([-0.05, -0.05, -np.pi * .75])
         self.ee_angle_highs = np.array([0.05, 0.05, np.pi * .75])
         self.ee_pos_lows = np.array([.671, -0.038, 0.195])
         self.ee_pos_highs = np.array([.787, .090, .270])
-        self.position_action_scale = .03 # max action is 2cm
+        self.position_action_scale = .03 # max action is 3cm
 
 
         # generate random goals
         if n_tasks == 1:
             self.goals = [np.array([.762, .0540, .1973])]
             # TODO fix me
-            self.goal_pose = np.array([.745, .052, .2071, .328, .944, .003, .023])
+            self.goal_pose = np.array([.745, .045, .236, .373, .928, -.006, .005])
 
             self._goal = self.goals[0]
 
@@ -53,8 +58,11 @@ class MultitaskSawyerPegEnv(Serializable, SawyerReachXYZEnv):
         #self.logger = logging.getLogger('exp.sawyer_peg_env')
 
     def _set_observation_space(self):
-        lows = np.array([.2, -.2, .03, -1, -1, -1, -1])
-        highs = np.array([.6, .2, .5, 1, 1, 1, 1])
+        # obs is full 7-dof pose + endpoint velocity
+        lows = np.array([.2, -.2, .03, -1, -1, -1, -1]) #, -1, -1, -1, -1, -1, -1])
+        highs = np.array([.6, .2, .5, 1, 1, 1, 1]) #, 1, 1, 1, 1, 1, 1])
+        #lows = np.array([.2, -.2, .03])
+        #highs = np.array([.6, .2, .5])
         self.observation_space = Box(lows, highs)
 
     def _set_action_space(self):
@@ -65,25 +73,32 @@ class MultitaskSawyerPegEnv(Serializable, SawyerReachXYZEnv):
         )
 
     def _get_obs(self):
-        # obs is full current ee pose: position + orientation
-        _, _, pose = self.request_observation()
-        return pose
+        # obs is full current ee pose: position + orientation (length 7)
+        # NOTE: obs also include ee velocity! (length 6)
+        _, _, ee_pose, ee_vel = self.request_observation()
+        return np.concatenate([ee_pose, ee_vel])
+
+    def _pose_from_obs(self, obs):
+        return obs[:7]
+
+    def _vel_from_obs(self, obs):
+        return obs[7:]
 
     def _move_by(self, action):
         # action consists of (x, y, z) position and rotation about the z axis
         # determine new desired pose
         # full pose is (x, y, z) + rot quaternion
-        ee_full_pose = self._get_obs()
+        ee_full_pose = self._pose_from_obs(self._get_obs())
 
         # first deal with the position part
         pos_act = action[:3] * self.position_action_scale
         ee_pos = ee_full_pose[:3]
         target_ee_pos = pos_act + ee_pos
-        old_ee_pos = target_ee_pos
+        old_ee_pos = np.copy(target_ee_pos)
         target_ee_pos = np.clip(target_ee_pos, self.ee_pos_lows, self.ee_pos_highs)
         #self.logger.info('\n POSITION')
-        #print('old', ee_pos)
-        #print('new', target_ee_pos)
+        print('old', ee_pos)
+        print('new', target_ee_pos)
         if np.any(old_ee_pos - target_ee_pos):
             print('safety box violated, position clipped')
 
@@ -117,6 +132,8 @@ class MultitaskSawyerPegEnv(Serializable, SawyerReachXYZEnv):
         quat_new = euler_2_quat(*new_eulers)
         #print('curr quat', ee_full_pose[3:])
         #print('new quat', quat_new)
+        # TODO no rotation for now
+        quat_new = self.goal_pose[3:]
         self._move_to(target_ee_pos, quat_new)
 
     def _move_to(self, target_pos, target_quat):
@@ -166,18 +183,28 @@ class MultitaskSawyerPegEnv(Serializable, SawyerReachXYZEnv):
         print('from:', self._get_obs()[:3])
         print('to:', self.reset_pose[:3])
         # return to fixed ee position and orientation
-        curr_error = self._get_obs() - self.reset_pose
-        # sometimes it takes more than one step to reset (perhaps sawyer controller has a timeout?) but always moving for a fixed number of times causes shaking
-        while np.any(np.abs(curr_error[:3]) > .005) or np.any(np.abs(curr_error[3:]) > .03):
+        curr_error = self._pose_from_obs(self._get_obs()) - self.reset_pose
+        counter = 1
+        while np.any(np.abs(curr_error[:3]) > .005) or np.any(np.abs(curr_error[3:]) > .03) or counter > 200:
             self._move_to(self.reset_pose[:3], self.reset_pose[3:])
-            curr_error = self._get_obs() - self.reset_pose
-            print(curr_error)
-        return self._get_obs()
+            # TODO: seems like we might need this while waiting for ROS to complete the action?
+            time.sleep(.05)
+            curr_error = self._pose_from_obs(self._get_obs()) - self.reset_pose
+            print('curr error', curr_error)
+            counter += 1
+        return self._pose_from_obs(self._get_obs())
 
     def step(self, action):
         # for now action is 4 DOF
+        print('before pose', self._get_obs()[:3])
+        s = time.time()
         self._move_by(action)
-        pose = self._get_obs()
+        # TODO: seems like we might need this while waiting for ROS to complete the action?
+        time.sleep(.05)
+        e = time.time()
+        obs = self._get_obs()
+        pose = self._pose_from_obs(obs)
+        print('after pose', pose[:3])
         # reward based on position only for now
         reward, points = self._compute_reward(action, pose)
         print('reward', reward)
