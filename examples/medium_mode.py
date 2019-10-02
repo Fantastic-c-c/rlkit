2787857e93be8c1964d4cf3df07d18a791370258
deq2 <deq2@cornell.edu>
2019-10-02 16:54:05 -0700

    vf changes to medium mode, and and remove 6dof from names

--- examples/medium_mode.py
+++ examples/medium_mode.py
@@ -36,7 +36,7 @@ def experiment(variant):
 
     obs_dim = int(np.prod(env.observation_space.shape))
     action_dim = int(np.prod(env.action_space.shape))
-    latent_dim = 7
+    latent_dim = 10
     task_enc_output_dim = latent_dim * 2 if variant['algo_params']['use_information_bottleneck'] else latent_dim
     reward_dim = 1
 
@@ -52,50 +52,49 @@ def experiment(variant):
             output_size=task_enc_output_dim,
     )
     qf1 = FlattenMlp(
-        hidden_sizes=[net_size, net_size, net_size],
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
         input_size=obs_dim + action_dim + latent_dim,
         output_size=1,
     )
     qf2 = FlattenMlp(
-        hidden_sizes=[net_size, net_size, net_size],
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
         input_size=obs_dim + action_dim + latent_dim,
         output_size=1,
     )
-    vf = FlattenMlp(
-        hidden_sizes=[net_size, net_size, net_size],
-        input_size=obs_dim + latent_dim,
+    target_qf1 = FlattenMlp(
+        input_size=obs_dim + action_dim + latent_dim,
         output_size=1,
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
+    )
+    target_qf2 = FlattenMlp(
+        input_size=obs_dim + action_dim + latent_dim,
+        output_size=1,
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
     )
     policy = TanhGaussianPolicy(
-        hidden_sizes=[net_size, net_size, net_size],
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
         obs_dim=obs_dim + latent_dim,
         latent_dim=latent_dim,
         action_dim=action_dim,
     )
 
     rf = FlattenMlp(
-        hidden_sizes=[net_size, net_size, net_size],
+        hidden_sizes=[net_size, net_size, net_size, net_size, net_size, net_size],
         input_size=obs_dim + action_dim + latent_dim,
         output_size=1
     )
 
-    alpha_network = FlattenMlp(
-        hidden_sizes=[64, 64],
-        input_size=num_tasks,
-        output_size=1,
-    )
-
     agent = ProtoAgent(
         latent_dim,
-        [task_enc, policy, qf1, qf2, vf, rf],
+        [task_enc, policy, qf1, qf2, target_qf1, target_qf2, rf],
         **variant['algo_params']
     )
 
     algorithm = ProtoSoftActorCritic(
         env=env,
-        train_tasks=list(tasks)[:10],
-        eval_tasks=list(tasks)[10:],
-        nets=[agent, task_enc, policy, qf1, qf2, vf, rf, alpha_network],
+        train_tasks=list(tasks),
+        eval_tasks=list(tasks),
+        nets=[agent, task_enc, policy, qf1, qf2, target_qf1, target_qf2, rf],
         latent_dim=latent_dim,
         **variant['algo_params']
     )
@@ -108,15 +107,15 @@ def experiment(variant):
 @click.argument('gpu', default=0)
 @click.option('--docker', default=0)
 def main(gpu, docker):
-    max_path_length = 150
+    max_path_length = 200
     # noinspection PyTypeChecker
     variant = dict(
         algo_params=dict(
             meta_batch=15,
             num_iterations=10000,
-            num_tasks_sample=15,
-            num_steps_per_task=10 * max_path_length,
-            num_train_steps_per_itr=4000,
+            num_tasks_sample=10,
+            num_steps_per_task=3 * max_path_length,
+            num_train_steps_per_itr=400,
             num_evals=5, # number of evals with separate task encodings
             num_steps_per_eval=10 * max_path_length,  # num transitions to eval on
             batch_size=256,  # to compute training grads from
@@ -129,7 +128,7 @@ def main(gpu, docker):
             qf_lr=3E-4,
             vf_lr=3E-4,
             context_lr=3e-4,
-            reward_scale=10.,
+            reward_scale=1.,
             sparse_rewards=False,
             reparameterize=True,
             kl_lambda=1,
@@ -150,7 +149,7 @@ def main(gpu, docker):
         gpu_id=gpu,
     )
 
-    exp_name = 'medium-rf1-deterministic-sampling-and-metabatch-kl1'
+    exp_name = 'medium-rf1-deterministic-sampling-and-metabatch-kl1-no-v'
 
     log_dir = '/mounts/output' if docker == 1 else 'output'
     experiment_log_dir = setup_logger(exp_name, variant=variant, exp_id='metaworld', base_log_dir=log_dir)
